9db8ed785d65d1f3b5d5a088f9c7a7d0153640d6
roboclown <40514109+roboclown@users.noreply.github.com>
2018-06-24 12:13:08 -0600

    Updated MenuController with functioning Move button and correct placement. Set new values on prefabs to support this.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,6 +8,8 @@ public class MenuController : MonoBehaviour {
 	public GameObject MenuCanvas;
 
     private UnitController currentUnit;
+    private float inputWait = 0.0f;
+    private bool selectingMovement = false;
 
     public void ActivateMenuOnUnit(UnitController unit)
     {
@@ -18,6 +20,24 @@ public class MenuController : MonoBehaviour {
         ac.SetTarget(unit.transform);
         MenuCanvas.SetActive(true);
     }
+
+    public void HideMenu()
+    {
+        Camera.main.GetComponentInParent<AutoCam>().enabled = false;
+        Camera.main.GetComponentInParent<CameraController>().enabled = true;
+        MenuCanvas.SetActive(false);
+    }
+
+    public void MoveCurrentUnit()
+    {
+        if(currentUnit != null )
+        {
+            selectingMovement = true;
+            HideMenu();
+            inputWait = Time.time + 0.1f;
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
 		
@@ -26,11 +46,30 @@ public class MenuController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		if( currentUnit != null )
+        if (currentUnit == null)
         {
-            RectTransform rt = MenuCanvas.GetComponent<RectTransform>();
-            //Vector3 menuPoint = currentUnit.transform.position + (Vector3)currentUnit.MenuOffset;
-            rt.anchoredPosition = Camera.main.WorldToScreenPoint(currentUnit.gameObject.transform.TransformPoint(currentUnit.gameObject.transform.position));
+            return;
         }
-	}
+
+        if(MenuCanvas.activeSelf)
+        {
+            var rt = MenuCanvas.GetComponent<RectTransform>();
+            var menuPoint = currentUnit.transform.position + (Vector3)currentUnit.MenuOffset;
+            MenuCanvas.transform.position = Camera.main.WorldToScreenPoint(menuPoint);
+            return;
+        }
+
+        if(Time.time < inputWait)
+        {
+            return;
+        }
+
+        if(selectingMovement && Input.GetMouseButtonDown(0))
+        {
+            var point = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var grid = GameManager.Instance.World2Grid(point);
+            currentUnit.Move(grid);
+            selectingMovement = false;
+        }
+    }
 }
